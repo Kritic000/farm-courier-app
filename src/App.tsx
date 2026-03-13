4dc1b73a2ad6a1fbdc9f32e3f462d5e6f126b322
Kritic000 <greencontinenttg@gmail.com>
2026-03-13 21:40:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbzKQ2DChUfqo-j2lB1LQH07epwav91vrjYoCSwFoKgsNbqgbgFKUVRNfTFiS78i7Hk/exec";
+  "https://script.google.com/macros/s/AKfycbwKL0PlYsotO_byhF-CZwFcmlci0bRSb4oyYtPIXB4xbHOqh7Q0WOwJI0TRLx4_P1w/exec";
 const API_TOKEN = "Kjhytccb18@";
 
 type Order = {
